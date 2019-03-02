847cd3981f3f4aac01cbfbac81ad869f5a22455c
tomas.langer <avatarek78@gmail.com>
2019-03-02 17:19:45 +0100

    first commit

---
+++ ppmt/src/main/java/io/agileinteligence/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,15 @@
+package io.agileinteligence.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+    public static void main(String[] args) {
+        // TODO: https://www.udemy.com/full-stack-project-spring-boot-20-react-redux/learn/v4/t/lecture/11795316?start=0
+        // 6. čas 7:00
+        SpringApplication.run(PpmtoolApplication.class, args);
+    }
+
+}
